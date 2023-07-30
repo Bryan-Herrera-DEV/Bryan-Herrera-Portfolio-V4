9673b64cec46facafdf705ae2a2ea0858e6ce4b5
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2023-07-30 16:27:05 -0500

    feat: :sparkles: add web banner

--- src/components/PageSeo.tsx
+++ src/components/PageSeo.tsx
@@ -25,16 +25,52 @@ export const PageSeo = ({ title, description, keywords }: PageSeoProps) => {
         content={`${inmutableConstants.NOMBRE_COMPLETO}, ${inmutableConstants.NOMBRE_COMPLETO}`}
       />
       <meta name="reply-to" content={`${inmutableConstants.EMAIL}`} />
-      <meta name="copyright" content={`${inmutableConstants.NOMBRE_COMPLETO}`} />
+      <meta
+        name="copyright"
+        content={`${inmutableConstants.NOMBRE_COMPLETO}`}
+      />
       <meta name="designer" content={`${inmutableConstants.NOMBRE_COMPLETO}`} />
 
       <title>{title}</title>
 
-      <meta name="robots" content="follow, index" />
       <meta name="description" content={description || ""} />
 
-      <meta name="keywords" content={Array.isArray(keywords) ? keywords.join(",") : keywords!} />
-      <meta name="theme-color" content="#0a0a0d"/>
+      <meta
+        name="keywords"
+        content={Array.isArray(keywords) ? keywords.join(",") : keywords!}
+      />
+      <meta name="theme-color" content="#0a0a0d" />
+
+      <meta name="robots" content="all" />
+      <meta name="googlebot-news" content="all" />
+      <meta
+        name="google-site-verification"
+        content="VFIoBukbxlXNN3cDw3_aaIGXvk0LbfAB1O9l7Ii6Wzg"
+      />
+      <meta name="url" content="https://bryan-herrera.netlify.app/" />
+      <meta
+        name="identifier-URL"
+        content="https://bryan-herrera.netlify.app/"
+      />
+      <link rel="canonical" href="https://bryan-herrera.netlify.app/" />
+
+      {/* Og */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://bryan-herrera.netlify.app/" />
+      <meta
+        property="og:title"
+        content={title}
+      />
+      <meta
+        property="og:description"
+        content={description}
+      />
+      <meta property="og:image:width" content="1584" />
+      <meta property="og:image:height" content="396" />
+      <meta
+        property="og:image"
+        content="/Bryan_Herrera_Banner.png"
+      />
     </Head>
   );
 };
