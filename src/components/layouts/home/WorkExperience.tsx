0f374f0b1c3f057478718b4291386acfcd480dd3
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2023-07-30 17:08:15 -0500

    refac: :hammer: add work experience

--- src/components/layouts/home/WorkExperience.tsx
+++ src/components/layouts/home/WorkExperience.tsx
@@ -5,7 +5,7 @@ const WorkExperience = () => {
   return (
     <section className="relative mb-4 text-center">
       <h2 className="inline bg-gradient-to-r from-baseRedColor via-indigo-200  to-baseYellowColor bg-clip-text font-display text-5xl tracking-tight text-transparent mb-5">
-        My Work Experience
+        My last 4 work experiences
       </h2>
       <GanttChart experiences={experiences} />
     </section>
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -61,8 +61,8 @@ export const TAGS: string[] = [
  * @description Constantes del workexperience
  */
 export const experiences: [string, Date, Date][] = [
-    ["Experiencia 1", new Date("2019-02"), new Date("2020-07")],
-    ["Experiencia 2", new Date("2020-08"), new Date("2021-07")],
-    ["Experiencia 5.5", new Date("2021-07"), new Date("2022-07")],
-    ["Experiencia 3\njaja", new Date("2019-07"), new Date("2023-11")]
+    ["EME - Senior Full Stack Developer", new Date("2021-11"), new Date("2022-07")],
+    ["Fulltimeforce - Senior Full Stack Developer", new Date("2022-10"), new Date("2023-05")],
+    ["MTC CORP - Senior Full Stack Developer", new Date("2023-05"), new Date()],
+    ["Freelance Dev", new Date("2020-01"), new Date()]
 ];
